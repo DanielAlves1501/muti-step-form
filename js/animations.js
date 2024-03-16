fac8dba1f19d467b673e43d465a33535a29a3a20
Daniel Alves <danielgsmax@gmail.com>
2024-03-16 14:34:25 -0400

    the payment mode (monthly/yearly) is now being saved in LocaleStorage

--- js/animations.js
+++ js/animations.js
@@ -2,6 +2,29 @@ import { handlePlanSelected } from "./index.js";
 
 let planInfo = {};
 
+const handlePlanBtnInitValue = (monthlyPlan,planBtn,yearlyPlan,cardPrice)=>{
+  if(planInfo.paymentMode == 'monthly'){
+    yearlyPlan.classList.remove("payment__mode--active");
+    planBtn.style.transform = "translateX(-70%)";
+    monthlyPlan.classList.add("payment__mode--active");
+
+    cardPrice[0].textContent = "$9/mo";
+    cardPrice[1].textContent = "$12/mo";
+    cardPrice[2].textContent = "$15/mo";
+
+  } else{
+    monthlyPlan.classList.remove("payment__mode--active");
+    planBtn.style.transform = "translateX(70%)";
+    yearlyPlan.classList.add("payment__mode--active");
+
+    cardPrice[0].textContent = "$90/yr";
+    cardPrice[1].textContent = "$120/yr";
+    cardPrice[2].textContent = "$150/yr";
+
+   
+  }
+}
+
 const step2Animations = () => {
   setTimeout(() => {
     const planBtn = document.querySelector(".payment__btn");
@@ -10,32 +33,49 @@ const step2Animations = () => {
     const cards = document.querySelectorAll(".card");
     const cardPrice = document.querySelectorAll(".card__price");
 
+    handlePlanBtnInitValue(monthlyPlan,planBtn,yearlyPlan,cardPrice)
+
+
     planBtn.addEventListener("click", (e) => {
       e.preventDefault();
+
+      let planStoredData = localStorage.getItem("planInformation");
+      planInfo = JSON.parse(planStoredData);
+
 
       if (planBtn.style.transform == "translateX(-70%)") {
         monthlyPlan.classList.remove("payment__mode--active");
         planBtn.style.transform = "translateX(70%)";
         yearlyPlan.classList.add("payment__mode--active");
+
+        cardPrice[0].textContent = "$90/yr";
+        cardPrice[1].textContent = "$120/yr";
+        cardPrice[2].textContent = "$150/yr";
+
+
+        planInfo = {...planInfo,paymentMode: "yearly"};
+
+        localStorage.setItem("planInformation",JSON.stringify(planInfo));
+        console.log(localStorage);
+
       } else {
         yearlyPlan.classList.remove("payment__mode--active");
         planBtn.style.transform = "translateX(-70%)";
         monthlyPlan.classList.add("payment__mode--active");
-      }
 
-      if (planBtn.style.transform == "translateX(-70%)") {
         cardPrice[0].textContent = "$9/mo";
         cardPrice[1].textContent = "$12/mo";
         cardPrice[2].textContent = "$15/mo";
-      } else {
-        cardPrice[0].textContent = "$90/yr";
-        cardPrice[1].textContent = "$120/yr";
-        cardPrice[2].textContent = "$150/yr";
+
+        planInfo = {...planInfo,paymentMode: "monthly"};
+
+        localStorage.setItem("planInformation",JSON.stringify(planInfo));
+        console.log(localStorage);
       }
     });
 
     cards.forEach((card) => {
-      const planStoredData = localStorage.getItem("planInformation");
+      let planStoredData = localStorage.getItem("planInformation");
 
       if (planStoredData) {
         planInfo = JSON.parse(planStoredData);
@@ -52,7 +92,7 @@ const step2Animations = () => {
         // Agregar la clase card--active solo a la card clickeada
         card.classList.add("card--active");
 
-        planInfo = { plan: card.childNodes[3].firstElementChild.innerText };
+        planInfo = { plan: card.childNodes[3].firstElementChild.innerText, price:card.childNodes[3].childNodes[3].innerText};
 
         localStorage.setItem("planInformation", JSON.stringify(planInfo));
         console.log(localStorage);
